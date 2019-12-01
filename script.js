504639d12d3635580d22047fc619e53c8f80e687
tagine <rana@ranafayez.com>
2019-12-01 02:06:12 -0500

    js vars and prompts added

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+var specialChar = prompt("Enter a special character")
+
+var numericChar = prompt("Enter a numeric character")
+
+var lowercaseChar = prompt("Enter a lowercase character")
+
+var uppercaseChar = prompt("Enter an uppercase character")
+
+alert(specialChar+numericChar+lowercaseChar+uppercaseChar)
+
