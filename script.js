bcd50c525f872e3361814aaf8e8e8b9e0c4784fe
tagine <rana@ranafayez.com>
2019-12-02 21:57:01 -0500

    more conditions added and more variables declared

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 
 //VARIABLE DECLARATIONS//
 
-var smallLetters = "abcdefghijklmnopqrstuvwxyz";
-var capletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var num ="0123456789";
+var lowCaseList = "abcdefghijklmnopqrstuvwxyz";
+var capCaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numList = "0123456789";
 var specialList = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var passlength=prompt("enter length of password (8-12)");
-console.log(passlength)
+var passlength = prompt("enter length of password (8-12)");
+console.log(passlength);
 
 var retVal = "";
 
-var specialChar = confirm("Do you need special char to be your passowrd?")
+var lowCaseChar = confirm("Would you like to include a lower case letter in your password?")
+var capCaseChar = confirm("Would you like to include a capital case letter in your password?")
+var numChar = confirm("Would you like to include a number in your password?")
+var specialChar = confirm("Would you like to include a special character in your password?")
+
 console.log(specialChar);
+
 // var numericChar = prompt("Enter a numeric character")
 
 // var lowercaseChar = prompt("Enter a lowercase character")
@@ -22,27 +27,39 @@ console.log(specialChar);
 
 
 // GENERATOR //
-if(specialChar === true){
-console.log("in specialchar")
+if (specialChar === true) {
+    console.log("in specialchar")
 
-for (var i = 0, n = specialList.length; i < passlength; ++i) {
+    for (var i = 0, n = lowCaseList.length; i < passlength; ++i) {
+        retVal += lowCaseList.charAt(Math.floor(Math.random() * n));
+        console.log(retVal)
+
+    }
+}
+else if (var i = 0, n = capCaseList.length; i < passlength; ++i) {
+    retVal += capCaseList.charAt(Math.floor(Math.random() * n));
+    console.log(retVal)
+
+    }
+
+}
+else if (var i = 0, n = numList.length; i < passlength; ++i) {
+    retVal += numList.charAt(Math.floor(Math.random() * n));
+    console.log(retVal)
+
+    }
+
+}
+else if (var i = 0, n = specialList.length; i < passlength; ++i) {
     retVal += specialList.charAt(Math.floor(Math.random() * n));
-console.log(retVal)
-   
-}
-}
-else if (){
+    console.log(retVal)
 
-
-}else if (){
-
-}else if(){
-
+    }
 }
 
 //PRINT PASSWORD//
 
-document.getElementById("p1").innerHTML = "hey this is me"
+document.getElementsByTagName("textarea").innerHTML = password"
 
 
 
