6f63006299ca7c5a80506f6c38fe3594a080b84b
tagine <rana@ranafayez.com>
2019-12-02 21:14:58 -0500

    more variables declared and arguments added

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 
 //VARIABLE DECLARATIONS//
 
-var specialChar = prompt("Enter a special character")
+var smallLetters = "abcdefghijklmnopqrstuvwxyz";
+var capletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num ="0123456789";
+var specialList = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var passlength=prompt("enter length of password (8-12)");
+console.log(passlength)
 
-var numericChar = prompt("Enter a numeric character")
+var retVal = "";
 
-var lowercaseChar = prompt("Enter a lowercase character")
+var specialChar = confirm("Do you need special char to be your passowrd?")
+console.log(specialChar);
+// var numericChar = prompt("Enter a numeric character")
 
-var uppercaseChar = prompt("Enter an uppercase character")
+// var lowercaseChar = prompt("Enter a lowercase character")
 
-alert(specialChar+numericChar+lowercaseChar+uppercaseChar)
+// var uppercaseChar = prompt("Enter an uppercase character")
+
+//alert(specialChar+numericChar+lowercaseChar+uppercaseChar)
 
 
 // GENERATOR //
+if(specialChar === true){
+console.log("in specialchar")
 
-function* gen() { 
-    yield 1;
-    yield 2;
-    yield 3;
-  }
-  
-  var g = gen(); // "Generator { }"
+for (var i = 0, n = specialList.length; i < passlength; ++i) {
+    retVal += specialList.charAt(Math.floor(Math.random() * n));
+console.log(retVal)
+   
+}
+}
+else if (){
+
+
+}else if (){
+
+}else if(){
+
+}
+
+//PRINT PASSWORD//
+
+document.getElementById("p1").innerHTML = "hey this is me"
+
+
+
+
 
 
