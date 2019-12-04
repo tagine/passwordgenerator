9a78b1280c5edf76acc6f92202193c41fd1858ad
tagine <rana@ranafayez.com>
2019-12-04 18:45:42 -0500

    cleaned up variables

--- script.js
+++ script.js
@@ -5,58 +5,83 @@ var lowCaseList = "abcdefghijklmnopqrstuvwxyz";
 var capCaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numList = "0123456789";
 var specialList = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var passlength = prompt("enter length of password (8-12)");
-console.log(passlength);
-
-var retVal = "";
+var passlength = prompt("enter length of password (8-126)");
 
 var lowCaseChar = confirm("Would you like to include a lower case letter in your password?")
 var capCaseChar = confirm("Would you like to include a capital case letter in your password?")
 var numChar = confirm("Would you like to include a number in your password?")
 var specialChar = confirm("Would you like to include a special character in your password?")
 
-console.log(specialChar);
+// var passwordCombo = lowCaseList + capCaseList + numList + specialList
 
-// var numericChar = prompt("Enter a numeric character")
 
-// var lowercaseChar = prompt("Enter a lowercase character")
+// if lowCaseChar (true) {
 
-// var uppercaseChar = prompt("Enter an uppercase character")
+//     passwordCombo = lowCaseList
+// }
 
-//alert(specialChar+numericChar+lowercaseChar+uppercaseChar)
+
+
+
+// var number = 1 + 2 + 3 + 4
+
+
+// var number = 0;
+// number = number + 1;
+// number = number + 2;
+// number = number + 3;
+// number = number + 4;
+
+// console.log(passlength);
+
+var retVal = "";
+
+
+
+// console.log(specialChar);
+
+// if (lowCaseList && capCaseList && numChar && specialChar) {
+
+// } 
 
 
 // GENERATOR //
-if (specialChar === true) {
-    console.log("in specialchar")
 
-    for (var i = 0, n = lowCaseList.length; i < passlength; ++i) {
-        retVal += lowCaseList.charAt(Math.floor(Math.random() * n));
-        console.log(retVal)
-
-    } 
-
-else if (var i = 0, n = capCaseList.length; i < passlength; ++i) {
-        retVal += capCaseList.charAt(Math.floor(Math.random() * n));
-        console.log(retVal)
-
-    }
-
-
-    // else if (var i = 0, n = numList.length; i < passlength; ++i) {
-    //     retVal += numList.charAt(Math.floor(Math.random() * n));
-    //     console.log(retVal)
-
-    //     }
-
-
-    // else if (var i = 0, n = specialList.length; i < passlength; ++i) {
-    //     retVal += specialList.charAt(Math.floor(Math.random() * n));
-    //     console.log(retVal)
-
-    //     }
-
+if (specialChar i = 0; n == specialList.length; i < passlength; ++i) {
+      retVal += specialList.charAt(Math.floor(Math.random() * n));
+       console.log(retVal)
 }
+
+// if (specialChar === true) {
+//     console.log("in specialchar")
+
+//     for (var i = 0, n = lowCaseList.length; i < passlength; ++i) {
+//         retVal += lowCaseList.charAt(Math.floor(Math.random() * n));
+//         console.log(retVal)
+
+//     } 
+
+// else if (var i = 0, n = capCaseList.length; i < passlength; ++i) {
+//     retVal += capCaseList.charAt(Math.floor(Math.random() * n));
+//     console.log(retVal)
+
+//     }
+
+
+// else if (var i = 0, n = numList.length; i < passlength; ++i) {
+//     retVal += numList.charAt(Math.floor(Math.random() * n));
+//     console.log(retVal)
+
+//     }
+
+
+// else if (var i = 0, n = specialList.length; i < passlength; ++i) {
+//     retVal += specialList.charAt(Math.floor(Math.random() * n));
+//     console.log(retVal)
+
+//     }
+
+// }
 //PRINT PASSWORD//
 
 document.getElementsByTagName("textarea").innerHTML = "password"
